230efc4a048645e7d78833f8d2cfbeee188cf5c7
Julian Vogel <julianvgl@web.de>
2019-09-23 16:56:04 +0200

    Restructuring
    New declaration of BCs, new structure of restructuring of subroutines, new subroutine for creation of system matrix, new comments

--- 1-NavierStokes-LidDrivenCavity/1-NS-LDC.py
+++ 1-NavierStokes-LidDrivenCavity/1-NS-LDC.py
@@ -29,16 +29,22 @@ ymin = 0.
 ymax = 2.0
 
 # Spatial discretization
-dx = 0.01
-dy = 0.01
+dx = 0.05
+dy = 0.05
 
 # Boundary conditions
-# Wall x-velocity: [W, E, S, N], NAN = symmetry
-uWall = [0., 0., 0., 1.]
-# Wall y-velocity: [W, E, S, N], NAN = symmetry
-vWall = [0., 0., 0., 0.]
-# Wall pressure: [W, E, S, N], NAN = symmetry
-pWall = [NAN, NAN, NAN, NAN]
+# Wall x-velocity: [[S, N]
+#                   [W, E]], NAN = symmetry
+uWall = [[0., 1.],
+         [0., 0.]]
+# Wall y-velocity: [[S, N]
+#                   [W, E]], NAN = symmetry
+vWall = [[0., 0.],
+         [0., 0.]]
+# Wall pressure: [[S, N]
+#                 [W, E]], NAN = symmetry
+pWall = [[NAN, NAN],
+         [NAN, NAN]]
 
 # Material properties
 rho = 1.0
@@ -47,13 +53,13 @@ nu = mu/rho
 
 # Temporal discretization
 tMax = 1.
-dt0 = 0.00025
+dt0 = 0.001
 
 # Solver
 amg_pre = True
 
 # Visualization
-dtOut = 0.01  # Output step length
+dtOut = 1.0  # Output step length
 dtPlot = 1.0  # Plot step length
 inlineGraphics = True
 nOut = int(round(tMax/dtOut))
@@ -82,7 +88,7 @@ p = np.zeros((ny+2, nx+2))
 b = np.zeros((ny, nx))
 bc = np.zeros((ny, nx))
 
-# Functions
+# Functions ====================================================================
 
 
 def avg(array, axis):
@@ -166,42 +172,34 @@ def animateContoursAndVelocityVectors(plotLevels1, plotLevels2, figureSize,
         ax1.quiver(X[1:-1, 1:-1][::m, ::m], Y[1:-1, 1:-1][::m, ::m],
                    uc[1:-1, :][::m, ::m], vc[:, 1:-1][::m, ::m])
 
-    return fig, ax1, ax2
-
     if saveFiguresToFile:
         formattedFilename = '{0}_{1:5.3f}.png'.format(outputFilename, t)
         path = Path('out') / formattedFilename
         plt.savefig(path)
 
+    return fig, ax1, ax2
 
-def setVelocityBoundaries(u, v, uWall, vWall):
 
-    # West
-    u[:, 0] = uWall[0]
-    if np.isnan(vWall[0]):
-        v[:, 0] = v[:, 1]  # symmetry
-    else:
-        v[:, 0] = 2*vWall[0] - v[:, 1]  # wall
-    # East
-    u[:, -1] = uWall[1]
-    if np.isnan(vWall[1]):
-        v[:, -1] = v[:, -2]  # symmetry
-    else:
-        v[:, -1] = 2*vWall[1] - v[:, -2]  # wall
-    # South
-    v[0, :] = vWall[2]
-    if np.isnan(uWall[2]):
-        u[0, :] = u[1, :]  # symmetry
-    else:
-        u[0, :] = 2*uWall[2] - u[1, :]  # wall
-    # North
-    v[-1, :] = vWall[3]
-    if np.isnan(uWall[3]):
-        u[-1, :] = u[-2, :]  # symmetry
-    else:
-        u[-1, :] = 2*uWall[3] - u[-2, :]  # wall
+def build1dpoissonmatrix(n, direction, wallBC):
+    # Poisson Matrix 1D
 
-    return u, v
+    # Neighbor coefficients
+    en = np.ones((1, n))
+    # Point coefficients
+    ep = -2*np.ones((1, n))
+    # South/West boundary
+    if np.isnan(wallBC[direction][0]):
+        ep[:, 0] += 1  # Neumann boundary condition
+    else:
+        ep[:, 0] += -1  # Dirichlet BC for boundary between nodes
+    # North/East boundary
+    if np.isnan(wallBC[direction][1]):
+        ep[:, -1] += 1  # Neumann boundary condition
+    else:
+        ep[:, -1] += -1  # Dirichlet BC for boundary between nodes
+    # return 1D coefficient matrix
+    return sps.spdiags((ep.ravel(), en.ravel(), en.ravel()),
+                       [0, 1, -1], (n), (n))
 
 
 def solveMomentumEquation(u, v, un, vn, dt, dx, dy, nu):
@@ -239,35 +237,87 @@ def solveMomentumEquation(u, v, un, vn, dt, dx, dy, nu):
     return u, v
 
 
-def solvePoissonEquation(p, pn, b, rho, dt, dx, dy, u, v, pWall, amg_pre):
+def correctPressure(p, pn, b, bc, rho, dt, dx, dy, u, v, pWall, amg_pre, Ap, M):
+
+    # Poisson equation
 
     # Right hand side
     b[:, :] = rho/dt*(np.diff(u[1:-1, :], axis=1)/dx +
                       np.diff(v[:, 1:-1], axis=0)/dy) + bc[:, :]
-
     # Solve linear system
     if amg_pre:
         [p[1:-1, 1:-1].flat[:], info] = spla.bicgstab(
-            A, b.flat[:], x0=p[1:-1, 1:-1].flat[:], tol=1e-9, maxiter=100, M=M)
-
-#        ml.solve(b=b,x0=x0,tol=1e-10,residuals=residuals,accel='cg')
-#        p[1:-1, 1:-1].flat[:] = ml.solve(b=b.flat[:], x0=p[1:-1, 1:-1].flat[:],
-#                                         tol=1e-9, accel='cg')
-
+            Ap, b.ravel(), x0=p[1:-1, 1:-1].ravel(), tol=1e-9, maxiter=100, M=M)
     else:
-        p[1:-1, 1:-1].flat[:] = spla.spsolve(A, b.flat[:])
+        p[1:-1, 1:-1].flat[:] = spla.spsolve(Ap, b.ravel())
 
-    return p
-
-
-def correctPressure(u, v, p, rho, dt, dx, dy):
-
+    # Project corrected pressure on velocity field
     u[1:-1, 1:-1] = u[1:-1, 1:-1] - dt/rho * \
         (p[1:-1, 2:-1]-p[1:-1, 1:-2])/(dx)
     v[1:-1, 1:-1] = v[1:-1, 1:-1] - dt/rho * \
         (p[2:-1, 1:-1]-p[1:-2, 1:-1])/(dy)
 
-    return u, v
+    return u, v, p
+
+
+def setBoundaryConditions(u, v, p, uWall, vWall, pWall):
+
+    # Velocities
+
+    # West
+    u[:, 0] = uWall[1][0]
+    if np.isnan(vWall[1][0]):
+        v[:, 0] = v[:, 1]  # symmetry
+    else:
+        v[:, 0] = 2*vWall[1][0] - v[:, 1]  # wall
+    # East
+    u[:, -1] = uWall[1][1]
+    if np.isnan(vWall[1][1]):
+        v[:, -1] = v[:, -2]  # symmetry
+    else:
+        v[:, -1] = 2*vWall[1][1] - v[:, -2]  # wall
+    # South
+    v[0, :] = vWall[0][0]
+    if np.isnan(uWall[0][0]):
+        u[0, :] = u[1, :]  # symmetry
+    else:
+        u[0, :] = 2*uWall[0][0] - u[1, :]  # wall
+    # North
+    v[-1, :] = vWall[0][1]
+    if np.isnan(uWall[0][1]):
+        u[-1, :] = u[-2, :]  # symmetry
+    else:
+        u[-1, :] = 2*uWall[0][1] - u[-2, :]  # wall
+
+    # Pressure (Only needed for correct plotting)
+
+    # West
+    if np.isnan(pWall[1][0]):
+        p[1:-1, 0] = p[1:-1, 1]
+    else:
+        p[1:-1, 0] = 2*pWall[1][0] - p[1:-1, 1]
+    # East
+    if np.isnan(pWall[1][1]):
+        p[1:-1, -1] = p[1:-1, -2]
+    else:
+        p[1:-1, -1] = 2*pWall[1][1] - p[1:-1, -2]
+    # South
+    if np.isnan(pWall[0][0]):
+        p[0, 1:-1] = p[1, 1:-1]
+    else:
+        p[0, 1:-1] = 2*pWall[0][0] - p[1, 1:-1]
+    # North
+    if np.isnan(pWall[0][1]):
+        p[-1, 1:-1] = p[-2, 1:-1]
+    else:
+        p[-1, 1:-1] = 2*pWall[0][1] - p[-2, 1:-1]
+    # Corners interpolated with mean of x- and y- 2nd order backward stencil
+    p[0, 0] = (2*p[0, 1] - p[0, 2] + 2*p[1, 0] - p[2, 0]) / 2  # SW
+    p[0, -1] = (2*p[0, -2] - p[0, -3] + 2*p[1, -1] - p[2, -1]) / 2  # SE
+    p[-1, 0] = (2*p[-1, 1] - p[-1, 2] + 2*p[-2, 0] - p[-3, 0]) / 2  # NW
+    p[-1, -1] = (2*p[-1, -2] - p[-1, -3] + 2*p[-2, -1] - p[-3, -1]) / 2  # NE
+
+    return u, v, p
 
 
 def calcDerived(Xf, Yf, p, u, v, dx, dy, nu, dt0):
@@ -285,13 +335,13 @@ def calcDerived(Xf, Yf, p, u, v, dx, dy, nu, dt0):
     # Divergence
     divU = np.diff(u[1:-1, :], axis=1)/np.diff(Xf[1:, :], axis=1) + \
         np.diff(v[:, 1:-1], axis=0)/np.diff(Yf[:, 1:], axis=0)
-    # Peclet numbers
+    # Peclet number
     Pe_u = uMax*dx/nu
     Pe_v = vMax*dy/nu
-    # Courant Friedrichs Levy numbers
+    # Courant Friedrichs Levy number
     CFL_u = uMax*dt0/dx
     CFL_v = vMax*dt0/dy
-    # Viscosity constraint
+    # Viscous time step constraint
     Vis_x = dt0*(2*nu)/dx**2
     Vis_y = dt0*(2*nu)/dy**2
 
@@ -299,110 +349,43 @@ def calcDerived(Xf, Yf, p, u, v, dx, dy, nu, dt0):
         Vis_x, Vis_y
 
 
-def interpolatePressureBoundaryValues(p, pWall):
+# PREPROCESSING ================================================================
 
-    # Pressure boundary values, for plotting of interpolated face values
-    # West
-    if np.isnan(pWall[0]):
-        p[1:-1, 0] = p[1:-1, 1]
-    else:
-        p[1:-1, 0] = 2*pWall[0] - p[1:-1, 1]
-    # East
-    if np.isnan(pWall[1]):
-        p[1:-1, -1] = p[1:-1, -2]
-    else:
-        p[1:-1, -1] = 2*pWall[1] - p[1:-1, -2]
-    # South
-    if np.isnan(pWall[2]):
-        p[0, 1:-1] = p[1, 1:-1]
-    else:
-        p[0, 1:-1] = 2*pWall[2] - p[1, 1:-1]
-    # North
-    if np.isnan(pWall[3]):
-        p[-1, 1:-1] = p[-2, 1:-1]
-    else:
-        p[-1, 1:-1] = 2*pWall[3] - p[-2, 1:-1]
-    # Corners
-    p[0, 0] = (2*p[0, 1] - p[0, 2] + 2*p[1, 0] - p[2, 0]) / 2  # SW
-    p[0, -1] = (2*p[0, -2] - p[0, -3] + 2*p[1, -1] - p[2, -1]) / 2  # SE
-    p[-1, 0] = (2*p[-1, 1] - p[-1, 2] + 2*p[-2, 0] - p[-3, 0]) / 2  # NW
-    p[-1, -1] = (2*p[-1, -2] - p[-1, -3] + 2*p[-2, -1] - p[-3, -1]) / 2  # NE
+# Poisson equation for pressure
 
-    return p
-
-
-# Poisson Matrix
-# Neighbor coefficients x-direction
-en = np.ones((1, nx))/(dx*dx)
-# Point coefficients x-direction
-ep = -2*np.ones((1, nx))
-# West boundary
-if np.isnan(pWall[0]):
-    ep[:, 0] += 1  # Neumann boundary condition
-else:
-    ep[:, 0] += -1  # Dirichlet BC for boundary between nodes
-# East boundary
-if np.isnan(pWall[1]):
-    ep[:, -1] += 1  # Neumann boundary condition
-else:
-    ep[:, -1] += -1  # Dirichlet BC for boundary between nodes
-ep = ep/(dx*dx)
-# 1D coefficient matrix x-direction
-Ax1d = sps.spdiags((ep.flat[:], en.flat[:], en.flat[:]),
-                   [0, 1, -1], (nx), (nx))
-# Identity matrix x-direction
-Iy = sps.eye(ny)
-# Full 2d coefficient matrix x-direction
-Ax = sps.kron(Iy, Ax1d)
-# Neighbor coefficients y-direction
-en = np.ones((1, ny))/(dy*dy)
-# Point coefficients y-direction
-ep = -2*np.ones((1, ny))
-# South boundary
-if np.isnan(pWall[2]):
-    ep[:, 0] += 1  # Neumann boundary condition
-else:
-    ep[:, 0] += -1  # Dirichlet BC for boundary between nodes
-# North boundary
-if np.isnan(pWall[3]):
-    ep[:, -1] += 1  # Neumann boundary condition
-else:
-    ep[:, -1] += -1  # Dirichlet BC for boundary between nodes
-ep = ep/(dy*dy)
-# 1D coefficient matrix y-direction
-Ay1d = sps.spdiags((ep.flat[:], en.flat[:], en.flat[:]),
-                   [0, 1, -1], (ny), (ny))
-# Identity matrix y-direction
+# Identity matrices
 Ix = sps.eye(nx)
-# Full 2d coefficient matrix y-direction
-Ay = sps.kron(Ay1d, Ix)
-# Full matrix both directions
-A = sps.csr_matrix(Ax + Ay)
-#  Zero pressure at boundary nodes in SW corner, if only Neumann boundaries
+Iy = sps.eye(ny)
+# 1D coefficient matrices x- and y-direction
+Apx1d = build1dpoissonmatrix(nx, 1, pWall)/(dx*dx)
+Apy1d = build1dpoissonmatrix(ny, 0, pWall)/(dy*dy)
+# Full 2d coefficient matrices x- and y-direction
+Apx = sps.kron(Iy, Apx1d)
+Apy = sps.kron(Apy1d, Ix)
+# Full 2d coefficient matrix
+Ap = sps.csr_matrix(Apx + Apy)
+# Set zero pressure at boundary nodes in SW corner, if only Neumann boundaries
 if np.all(np.isnan(pWall)):
-    A[0, 0] = 3/2*A[0, 0]
+    Ap[0, 0] = 3/2*Ap[0, 0]
+# Set Dirichlet BC: Constant part goes in RHS
+# West
+if not np.isnan(pWall[1][0]):
+    bc[:, 0] = -2*pWall[1][0]
+# East
+if not np.isnan(pWall[1][1]):
+    bc[:, -1] = -2*pWall[1][1]
+# South
+if not np.isnan(pWall[0][0]):
+    bc[0, :] = -2*pWall[0][0]
+# North
+if not np.isnan(pWall[0][1]):
+    bc[-1, :] = -2*pWall[0][1]
 
-# AMG Solver
+# Algebraic Multigrid (AMG) as preconditioner
 if amg_pre:
     import pyamg
-#    ml = pyamg.ruge_stuben_solver(A, max_coarse=10)
-    ml = pyamg.smoothed_aggregation_solver(A, max_coarse=10)
+    ml = pyamg.smoothed_aggregation_solver(Ap, max_coarse=10)
     M = ml.aspreconditioner(cycle='V')
-
-# Boundary Conditions of Poisson equation
-# Constant part of Dirichlet boundary conditions goes in RHS
-# West
-if not np.isnan(pWall[0]):
-    bc[:, 0] = -2*pWall[0]
-# East
-if not np.isnan(pWall[1]):
-    bc[:, -1] = -2*pWall[1]
-# South
-if not np.isnan(pWall[2]):
-    bc[0, :] = -2*pWall[2]
-# North
-if not np.isnan(pWall[3]):
-    bc[-1, :] = -2*pWall[3]
 
 # Calculate derived quantities
 [pf, uc, vc, U, uMax, vMax, divU, Pe_u, Pe_v, CFL_u, CFL_v, Vis_x, Vis_y] = \
@@ -412,7 +395,8 @@ if not np.isnan(pWall[3]):
 [fig, ax1, ax2] = animateContoursAndVelocityVectors(plotLevels1, plotLevels2,
                                                     figureSize)
 
-# Time stepping
+
+# Time stepping ================================================================
 twall0 = time.time()
 tOut = dtOut
 tPlot = dtPlot
@@ -432,16 +416,14 @@ while t < tMax:
     vn = v.copy()
 
     # Projection method
-    # Set velocity boundaries
-    [u, v] = setVelocityBoundaries(u, v, uWall, vWall)
+
     # Intermediate velocity field u*
     [u, v] = solveMomentumEquation(u, v, un, vn, dt, dx, dy, nu)
-    # Solve poisson equation
-    p = solvePoissonEquation(p, pn, b, rho, dt, dx, dy, u, v, pWall, amg_pre)
     # Pressure correction
-    [u, v] = correctPressure(u, v, p, rho, dt, dx, dy)
-    # Interpolate pressure
-    p = interpolatePressureBoundaryValues(p, pWall)
+    [u, v, p] = correctPressure(p, pn, b, bc, rho, dt, dx, dy, u, v, pWall,
+                                amg_pre, Ap, M)
+    # Update boundary values
+    [u, v, p] = setBoundaryConditions(u, v, p, uWall, vWall, pWall)
 
     if (t-tOut) > -1e-6:
 
@@ -456,7 +438,7 @@ while t < tMax:
               (uMax, CFL_u, Pe_u, Vis_x))
         print(" max|v| = %5.2e, CFL(v) = %5.2f, Pe(v) = %5.2f, Vis(y) = %5.2f" %
               (vMax, CFL_v, Pe_v, Vis_y))
-        print(" Residual: ", np.linalg.norm(b.flat[:]-A*p[1:-1, 1:-1].flat[:]))
+        print(" Residual: ", np.linalg.norm(b.ravel()-Ap*p[1:-1, 1:-1].ravel()))
 
         tOut += dtOut
 
